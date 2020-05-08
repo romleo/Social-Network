21a6da2d6ac2cbf217d68f5401a29410f8c36d7e
Roman <romandybinetc@gmail.com>
2020-05-08 17:47:56 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRkSKyyG9E-pkMlv8MEF2PxEI3AIDagkdMquT06KxOc45YIPYqx&usqp=CAU' alt=''/>
+        </header>
+    )
+    
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+         <div>
+            Main Content
+            <img src='https://www.sticky.digital/wp-content/uploads/2013/11/img-6.jpg' alt=''/>
+         </div>
+         <div>
+            avatar+my description
+         </div>
+         <div>
+            My posts
+            <div>
+              New Post
+            </div>
+              <div>
+                post 1
+              </div>
+              <div>
+                post 2
+              </div>
+         </div>
+        </div>
+    )
+    
+};
+
+export default Profile;
